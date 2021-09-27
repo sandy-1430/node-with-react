e1eb4931898a2c4cc0f9b94abdd87f8fc4e50c04
sandy-1430 <73515551+sandy-1430@users.noreply.github.com>
2021-09-28 01:20:00 +0530

    Update studentAction.js

--- src/actions/studentAction.js
+++ src/actions/studentAction.js
@@ -9,7 +9,7 @@ import {
 const studentResult = (sem, rollno) => async (dispatch) => {
     dispatch({ type: STUDENT_RESULT_REQUEST, payload: { sem } });
     try {
-        const { data } = await Axios.get("/result/" + sem + "/" + rollno + "");
+        const { data } = await Axios.get("https://sandy-node-restapi.herokuapp.com/result/" + sem + "/" + rollno + "");
         dispatch({ type: STUDENT_RESULT_SUCCESS, payload: data });
     } catch (error) {
         dispatch({ type: STUDENT_RESULT_FAIL, payload: error.message });
