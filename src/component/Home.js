6171a790ce7308167aba1a1cb0e555afe91db560
sandy-1430 <73515551+sandy-1430@users.noreply.github.com>
2021-10-23 15:19:10 +0530

    Update Home Splash Screen

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,33 +1,31 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
 
 export default function Home() {
     return (
-        <div id="carouselExampleControls" class="carousel slide my-5 pt-2" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="images/hero-bg.jpg" class="d-block w-100" alt="..." />
-                </div>
-                <div class="carousel-item">
-                    <img src="images/studentlogin.jpg" class="d-block w-100" alt="..." />
-                </div>
-                <div class="carousel-item">
-                    <img src="images/hero-bg.jpg" class="d-block w-100" alt="..." />
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
+       <Carousel>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="images/hero-bg.jpg"
+            alt="First slide"
+            />
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="images/studentlogin.jpg"
+            alt="Second slide"
+            />
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="images/hero-bg.jpg"
+            alt="Third slide"
+            />
+        </Carousel.Item>
+        </Carousel>
     )
 }
